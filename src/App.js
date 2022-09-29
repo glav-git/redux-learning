44d8431a1196ad7e0ead290c00d1e8b05f81fe1d
glav-git <alexeyholostenko96@gmail.com>
2022-09-30 00:35:29 +0300

    using dispatch to update cash state using buttons (fixed iterations)

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ function App() {
   const dispatch = useDispatch();
   // to get data from state u Need hook useSelector
   const cash = useSelector(state => state.cash);
-  console.log(cash)
   
+  const addCash = () => {
+    dispatch({type:"ADD_CASH", payload: 1})
+  }
 
+  const getCash = () => {
+    dispatch({type:"GET_CASH", payload: 1})
+  }
 
   return (
     <div style={{background: "pink", display: "flex", border: "1px solid black", width: "150px", height: "50px", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
-      <div> Balance: {useSelector(state => state.cash)}</div>
+      <div> Balance: {cash}</div>
       <div style={{width: "150px", display: "flex",justifyContent: "space-around"}}>
-        <button>Pop Up</button>
-        <button>Pop Down</button>
+        <button onClick={() => addCash()}>Pop Up</button>
+        <button onClick={() => getCash()}>Pop Down</button>
       </div>
     </div>
   );
