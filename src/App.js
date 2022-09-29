95752d28d81e68d2e9efd29c9cf2bd9c3c03701c
glav-git <alexeyholostenko96@gmail.com>
2022-09-29 23:57:59 +0300

    create buttons and show cash on screen using useSelector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  // to modify state in component u NEED dispatcher
+  const dispatch = useDispatch();
+  // to get data from state u Need hook useSelector
+  const cash = useSelector(state => state.cash);
+  console.log(cash)
+  
+
+
   return (
-    <div>
-      ochko negra
+    <div style={{background: "pink", display: "flex", border: "1px solid black", width: "150px", height: "50px", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+      <div> Balance: {useSelector(state => state.cash)}</div>
+      <div style={{width: "150px", display: "flex",justifyContent: "space-around"}}>
+        <button>Pop Up</button>
+        <button>Pop Down</button>
+      </div>
     </div>
   );
 }
